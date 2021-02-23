6b635448080dad91d2045576984b42619d763faf
sephirothmk <vladimir.totocevski@intertec.io>
2021-02-23 14:52:30 +0100

    feat(session_id) - removed all notion of session

--- framework/core/controller.js
+++ framework/core/controller.js
@@ -356,14 +356,7 @@ Controller = ControllerInterface.inherit({
      * @return {string}
      */
     getSession: function Controller_getSession(key) {
-        var session = component.get('storage/session'),
-            session_id = this.getCookie(session.getCookieKey());
-
-        if (Type.isString(key)) {
-            return session.get(session_id + key);
-        }
-
-        throw new error.HttpError(500, {key: key, session_id: session_id}, 'Controller.getSession: key must be string type');
+        return "";
     },
     /**
      * @since 0.0.1
@@ -377,15 +370,7 @@ Controller = ControllerInterface.inherit({
      * @return {string}
      */
     setSession: function Controller_setSession(key, value, refreshSessionId) {
-        var session = component.get('storage/session'),
-            session_id = this.getCookie(session.getCookieKey());
-        if (!Type.isString(key)) {
-            throw new error.HttpError(500, {key: key, session_id: session_id}, 'Controller.getSession: key must be string type');
-        } else if (!session_id || !!refreshSessionId) {
-            session_id = this.__requestApi__.uuid() + '_' + (new Date).getTime();
-            this.setCookie(session.getCookieKey(), session_id, session.getExpiredTime());
-        }
-        session.set(session_id + key, value);
+
     },
     /**
      * @since 0.0.1
@@ -397,14 +382,7 @@ Controller = ControllerInterface.inherit({
      * @return {string}
      */
     removeSession: function Controller_removeSession(key) {
-        var session = component.get('storage/session'),
-            session_id = this.getCookie(session.getCookieKey());
 
-        if (Type.isString(key)) {
-            return session.remove(session_id + key);
-        }
-
-        throw new error.HttpError(500, {key: key, session_id: session_id}, 'Controller.removeSession: key must be string type');
     },
     /**
      * @since 0.0.1
